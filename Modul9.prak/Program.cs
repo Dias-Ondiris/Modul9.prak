03577d1b6708d8690e80a5b7cf7a3c5e4d8cb21c
Dias-Ondiris <dias.ondiris2@gmail.com>
2023-11-26 21:25:45 +0600

    Добавьте файлы проекта.

---
+++ Modul9.prak/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9.prak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flash flashDrive = new Flash("Flash", "Flash-память", 100, 32); 
+            DVD dvd = new DVD("DVD", "DVD-диск", 10, true); 
+            HDD hdd = new HDD("HDD", "съемный HDD", 60, 4, 250); 
+            Storage[] storages = { flashDrive, dvd, hdd };
+            double dataToCopy = 565 * 1024; 
+            double totalMemory = 0;
+            foreach (Storage storage in storages)
+            {
+                totalMemory += storage.GetMemoryCapacity();
+            }
+            Console.WriteLine($"Общий объем памяти всех устройств: {totalMemory} Гб");
+            foreach (Storage storage in storages)
+            {
+                storage.CopyData(dataToCopy);
+            }
+            foreach (Storage storage in storages)
+            {
+                double numberOfDevicesNeeded = Math.Ceiling(dataToCopy / (storage.GetMemoryCapacity() * 1024));
+                Console.WriteLine($"Для переноса {dataToCopy} Мб на {storage.Name} потребуется {numberOfDevicesNeeded} устройств(а).");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modul9.prak/Storage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9.prak
+{
+    public abstract class Storage
+    {
+        private string name;
+        private string model;
+        protected double usedMemory;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public abstract double GetMemoryCapacity();
+        public abstract void CopyData(double dataAmount);
+        public abstract double GetFreeMemory();
+        public abstract string GetDeviceInfo();
+    }
+
+}
